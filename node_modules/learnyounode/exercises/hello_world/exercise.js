fc9481bcbf1a511c135c208e0f9ad1d0fe5b9a7a
sheerdyyypia <piasheerdydeasis@gmail.com>
2020-01-23 16:40:57 +0800

    get ignore commit

---
+++ node_modules/learnyounode/exercises/hello_world/exercise.js
@@ -0,0 +1,16 @@
+'use strict'
+let exercise = require('workshopper-exercise')()
+const filecheck = require('workshopper-exercise/filecheck')
+const execute = require('workshopper-exercise/execute')
+const comparestdout = require('workshopper-exercise/comparestdout')
+
+// checks that the submission file actually exists
+exercise = filecheck(exercise)
+
+// execute the solution and submission in parallel with spawn()
+exercise = execute(exercise)
+
+// compare stdout of solution and submission
+exercise = comparestdout(exercise)
+
+module.exports = exercise
